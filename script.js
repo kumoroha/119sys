af695f1b2f4a104bbfa633b870d95f1c5ad3e573
kumoroha <i3evo-core11G@outlook.com>
2025-03-24 16:46:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const map = L.map('map').setView([34.6937, 135.5023], 13); // 初期表示を大阪に設定
+    const map = L.map('map').setView([34.6937, 135.5023], 11); // 初期表示を大阪に設定し、ズーム度を11に設定
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
